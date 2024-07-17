d48370f6a12bfc7f5277831064c15cb1b3e50a66
swar186j <74865434+swar186j@users.noreply.github.com>
2024-07-17 17:58:33 +0530

    Update function_app.py

--- MyTriggerQ/function_app.py
+++ MyTriggerQ/function_app.py
@@ -6,11 +6,25 @@ import json
 import os
 from urllib.parse import unquote
 
-def install():
-    subprocess.check_call([sys.executable, "-m", "pip", "install","-r", "requirements.txt"])
-    logging.info("Successfully installed requirements module")
-install()
+# def install():
+#     subprocess.check_call([sys.executable, "-m", "pip", "install","-r", "requirements.txt"])
+#     logging.info("Successfully installed requirements module")
+# install()
+def install(package):
+    # This function will install a package if it is not present
+    from importlib import import_module
+    try:
+        import_module(package)
+    except:
+        from sys import executable as se
+        from subprocess import check_call
+        check_call([se,'-m','pip','-q','install',package])
+        logging.info("Successfully installed python modules")
+        
 
+
+for package in ['requests','azure-functions','azure-storage-blob']:
+    install(package)
 
         
 import requests
